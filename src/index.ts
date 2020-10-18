2996922ac99f3edcbcae2518e7966f757ac62dea
riggy <riley@sleepy.fish>
2020-10-17 23:45:00 -0400

    installed bogie

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js';
+import { World, Rectangle, Point } from 'bogie';
 import loader from './loader';
 import { Game } from './types';
 import { Ship } from './entity';
@@ -19,6 +20,7 @@ const app = new PIXI.Application({
 export const GAME: Game = {
   APP: app,
   LOADER: app.loader,
+  WORLD: new World(),
 };
 document.body.appendChild(app.view);
 
@@ -37,12 +39,23 @@ loader(
       y: window.innerHeight / 2,
       scale: 0.2,
     });
+    const rect = new Rectangle({
+      width: 100,
+      height: 50,
+    })
+      .position(new Point(200, 300))
+      .rotation(2)
+      .makeCollidable(GAME.WORLD)
+      .makeDebug(app.stage, 0x104496);
     window.addEventListener('resize', () => {
       app.renderer.resize(window.innerWidth, window.innerHeight);
       ship.position(new PIXI.Point(window.innerWidth / 2, window.innerHeight / 2));
     });
     ship.activate();
-    app.ticker.add(delta => ship.run(delta));
+    app.ticker.add(delta => {
+      ship.run(delta);
+      rect.run(delta);
+    });
   },
   (loader: any) => {
     console.info(`${Math.round(loader.progress)}% Loaded`);
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,8 @@
 import * as PIXI from 'pixi.js';
+import { World } from 'bogie';
 
 export interface Game {
   APP: PIXI.Application;
   LOADER: PIXI.Loader;
+  WORLD: World;
 }
